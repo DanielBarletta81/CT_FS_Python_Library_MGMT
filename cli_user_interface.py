dac4bff646ed6b06149bef72d5022cd52e84d57d
DanielBarletta81 <dbarcelona1481@gmail.com>
2024-10-28 13:39:01 -0400

    set up main menu for Library Management System app

---
+++ cli_user_interface.py
@@ -0,0 +1,28 @@
+#Create an improved, user-friendly command-line interface (CLI) for the Library Management
+#  System with separate menus for each class of the system.
+"""     Welcome to the Library Management System!
+
+    Main Menu:
+    1. Book Operations
+    2. User Operations
+    3. Author Operations
+    4. Quit """
+
+## main menu
+
+
+
+def main():
+  while True:
+    
+     print("***  Welcome to the Library Management System!  ***")
+     print("\n Menu: ")
+     print("\n 1. Book Operations. ")
+     print("\n 2. User Operations. ")
+     print("\n 3. Author Operations. ")
+     print("\n 4. Quit. ")
+
+
+main()   
+
+
