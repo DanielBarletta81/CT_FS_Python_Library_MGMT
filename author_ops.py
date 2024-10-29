e50293778a992237cd423e5423113c6f3b2022cb
DanielBarletta81 <dbarcelona1481@gmail.com>
2024-10-29 13:25:40 -0400

    create classes for User and Author, establish getters and setters

--- author_ops.py
+++ author_ops.py
@@ -3,3 +3,26 @@
         2. View author details
         3. Display all authors """
 
+class Author:
+  
+    def __init__(self, name, biography):
+        #make private attributes for author name and biography
+    
+        self.__name = name
+      
+        self.__biography = biography
+     
+    # getters and setters
+    def get_name(self):
+        return self.__name
+
+    def set_name(self, new_name):
+        self.__name = new_name
+
+    def get_biography(self):
+        return self.__biography
+
+    def set_biography(self, new_biography):
+        self.__biography = new_biography
+
+
