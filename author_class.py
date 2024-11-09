654e0f4778f39449fed3079a28710665b30af890
DanielBarletta81 <dbarcelona1481@gmail.com>
2024-11-09 16:52:39 -0500

    finish author class and menu

--- author_class.py
+++ author_class.py
@@ -2,10 +2,11 @@
 
 class Author:
   
-    def __init__(self, name, biography):
+    def __init__(self, author_id,  author_name, biography):
         #make private attributes for author name and biography
-    
-        self.__name = name
+
+        self.__author_id = author_id
+        self.__author_name = author_name
       
         self.__biography = biography
 
@@ -25,12 +26,10 @@ class Author:
 
       # getters and setters
    
-    def get_name(self):
-        return self.__name
+    def get_author_name(self):
+        return self.__author_name
 
-    def set_name(self, new_name):
-        self.__name = new_name
-
+    
     def get_biography(self):
         return self.__biography
 
@@ -46,10 +45,11 @@ def add_author(author_list):
     
      
 
-def get_author_details(author_id, author_list):
-        author = Author()
+def get_author_details(author_list):
+        author_id = int(input("Please enter the author id: "))
         if author_id in author_list:
-            print(f'Details for selected author: {author.__name} ')
+            print(f'Details for selected author: {author_list[author_id].get_author_name()} from their biography: {author_list[author_id].get_biography()}') 
+            
 
 
 def display_authors(author_list):
@@ -57,7 +57,7 @@ def display_authors(author_list):
         print("Current authors:")
         for author in author_list:
             if author:
-                print(author)
+                print(f'Author ID: {author} \n Name: {author_list[author].get_author_name()}')
 
 """  Author Operations:
         1. Add a new author
@@ -84,7 +84,7 @@ def authorMenu():
             add_author(author_list)
                 
         elif choice == 2:
-            pass
+            get_author_details(author_list)
                 
         elif choice == 3:
-            pass
+            display_authors(author_list)
